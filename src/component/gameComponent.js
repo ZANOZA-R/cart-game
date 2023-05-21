e70ec077432a16fa2305bd6ddcb857bb99312b83
hw1 <lilia_dali@mail.ru>
2023-05-22 04:06:46 +0700

    Инструменты для сборки проектов

--- src/component/gameComponent.js
+++ src/component/gameComponent.js
@@ -1,119 +1,57 @@
-import { globalData, mainelement } from "../index.js";
+import {
+    globalData,
+    mainelement,
+    getRandomInt,
+    randomButtonElements,
+} from "../index.js";
 import { difficultyLevel } from "./startingСomponent.js";
-const cardsPresetsClosed = `
-<div class="cards">
-        <div class="cards__title">
-            <div>
-                <div class="cards__time-box">
-                    <p class="cards__time">min</p>
-                    <p class="cards__time">sek</p>
-                </div>
-                <p class="cards__timer">00.00</p>
-            </div>
-            <button class="cards__button-again">Начать заново</button>
-        </div>
-        <div class="cards__button-box">
-        <button class="cards__button"></button>
-        <button class="cards__button"></button>
-        <button class="cards__button"></button>
-        <button class="cards__button"></button>
-        <button class="cards__button"></button>
-        <button class="cards__button"></button>
-        <button class="cards__button"></button>
-        <button class="cards__button"></button>
-        <button class="cards__button"></button>
-        <button class="cards__button"></button>
-        <button class="cards__button"></button>
-        <button class="cards__button"></button>
-        <button class="cards__button"></button>
-        <button class="cards__button"></button>
-        <button class="cards__button"></button>
-        <button class="cards__button"></button>
-        <button class="cards__button"></button>
-        <button class="cards__button"></button>
-        <button class="cards__button"></button>
-        <button class="cards__button"></button>
-        <button class="cards__button"></button>
-        <button class="cards__button"></button>
-        <button class="cards__button"></button>
-        <button class="cards__button"></button>
-        <button class="cards__button"></button>
-        <button class="cards__button"></button>
-        <button class="cards__button"></button>
-        <button class="cards__button"></button>
-        <button class="cards__button"></button>
-        <button class="cards__button"></button>
-        <button class="cards__button"></button>
-        <button class="cards__button"></button>
-        <button class="cards__button"></button>
-        <button class="cards__button"></button>
-        <button class="cards__button"></button>
-        <button class="cards__button"></button>
-        </div>
+const cardsTitle = `
+<div class="cards__title">
+<div>
+    <div class="cards__time-box">
+        <p class="cards__time">min</p>
+        <p class="cards__time">sek</p>
     </div>
+    <p class="cards__timer">00.00</p>
+</div>
+<button class="cards__button-again">Начать заново</button>
+</div>
 `;
-const cardsPresetsOpen = `
-<div class="cards">
-   <div class="cards__title">
-       <div>
-           <div class="cards__time-box">
-               <p class="cards__time">min</p>
-               <p class="cards__time">sek</p>
-           </div>
-           <p class="cards__timer">00.00</p>
-       </div>
-       <button class="cards__button-again">Начать заново</button>
-   </div>
-   <div class="cards__button-box">
-       <button class="cards__button"> <img src="./img/пики/туз пики.svg" alt=""></button>
-       <button class="cards__button"><img src="./img/пики/король пики.svg" alt=""></button>
-       <button class="cards__button"><img src="./img/пики/дама пики.svg" alt=""></button>
-       <button class="cards__button"><img src="./img/пики/валет пики.svg" alt=""></button>
-       <button class="cards__button"><img src="./img/пики/10 пики.svg" alt=""></button>
-       <button class="cards__button"><img src="./img/пики/9 пики.svg" alt=""></button>
-       <button class="cards__button"><img src="./img/пики/8 пики.svg" alt=""></button>
-       <button class="cards__button"><img src="./img/пики/7 пики.svg" alt=""></button>
-       <button class="cards__button"><img src="./img/пики/6 пики.svg" alt=""></button>
-       <button class="cards__button"><img src="./img/черви/туз черви.svg" alt=""></button>
-       <button class="cards__button"><img src="./img/черви/король черви.svg" alt=""></button>
-       <button class="cards__button"><img src="./img/черви/дама черви.svg" alt=""></button>
-       <button class="cards__button"><img src="./img/черви/валет черви.svg" alt=""></button>
-       <button class="cards__button"><img src="/img/черви/10 черви.svg" alt=""></button>
-       <button class="cards__button"><img src="./img/черви/9 черви.svg" alt=""></button>
-       <button class="cards__button"><img src="./img/черви/8 черви.svg" alt=""></button>
-       <button class="cards__button"><img src="./img/черви/7 черви.svg" alt=""></button>
-       <button class="cards__button"><img src="./img/черви/6 черви.svg" alt=""></button>
-       <button class="cards__button"><img src="./img/буби/туз бубны.svg" alt=""></button>
-       <button class="cards__button"><img src="./img/буби/король бубны.svg" alt=""></button>
-       <button class="cards__button"><img src="/img/буби/дама бубны.svg" alt=""></button>
-       <button class="cards__button"><img src="./img/буби/валет бубны.svg" alt=""></button>
-       <button class="cards__button"><img src="./img/буби/10 бубны.svg" alt=""></button>
-       <button class="cards__button"><img src="./img/буби/9 бубны.svg" alt=""></button>
-       <button class="cards__button"><img src="./img/буби/8 бубны.svg" alt=""></button>
-       <button class="cards__button"><img src="./img/буби/7 бубны.svg" alt=""></button>
-       <button class="cards__button"><img src="./img/буби/6 бубны.svg" alt=""></button>
-       <button class="cards__button"><img src="./img/крести/туз крести.svg" alt=""></button>
-       <button class="cards__button"><img src="./img/крести/король крести.svg" alt=""></button>
-       <button class="cards__button"><img src="./img/крести/дама крести.svg" alt=""></button>
-       <button class="cards__button"><img src="./img/крести/валет крести.svg" alt=""></button>
-       <button class="cards__button"><img src="./img/крести/10 крести.svg" alt=""></button>
-       <button class="cards__button"><img src="./img/крести/9 крести.svg" alt=""></button>
-       <button class="cards__button"><img src="./img/крести/8 крести.svg" alt=""></button>
-       <button class="cards__button"><img src="./img/крести/7 крести.svg" alt=""></button>
-       <button class="cards__button"><img src="./img/крести/6 крести.svg" alt=""></button>
-       </div>
-       </div>
-       `;
+
 function renderingPresetsCards() {
     switch (globalData.difficultyLevel) {
         case "easy":
-            mainelement.innerHTML = cardsPresetsClosed;
+            getRandomInt(3);
+            mainelement.innerHTML = `
+            <div  class="cards">
+                ${cardsTitle}
+                <div class="cards__button-box">
+                    ${randomButtonElements(6)}  
+                </div>
+            </div> 
+            `;
             break;
         case "average":
-            mainelement.innerHTML = cardsPresetsClosed;
+            getRandomInt(6);
+            mainelement.innerHTML = `
+            <div  class="cards">
+                ${cardsTitle}
+                <div class="cards__button-box">
+                    ${randomButtonElements(12)}  
+                </div>
+            </div> 
+            `;
             break;
         case "hard":
-            mainelement.innerHTML = cardsPresetsOpen;
+            getRandomInt(9);
+            mainelement.innerHTML = `
+            <div  class="cards">
+                ${cardsTitle}
+                <div class="cards__button-box">
+                    ${randomButtonElements(18)}  
+                </div>
+            </div> 
+            `;
             break;
         default:
             break;
@@ -121,7 +59,38 @@ function renderingPresetsCards() {
     document
         .querySelector(".cards__button-again")
         .addEventListener("click", () => {
+            delete globalData.selectedCard;
             difficultyLevel();
         });
+    checkButtonPreset();
+}
+function checkButtonPreset() {
+    const buttonElements = document.querySelectorAll(".cards__button");
+    setTimeout(() => {
+        for (const buttonElement of buttonElements) {
+            buttonElement.classList.remove(
+                `preset__${buttonElement.dataset.preset}`
+            );
+        }
+        for (const buttonElement of buttonElements) {
+            buttonElement.addEventListener("click", () => {
+                buttonElement.classList.add(
+                    `preset__${buttonElement.dataset.preset}`
+                );
+                if (!globalData.selectedCard) {
+                    globalData.selectedCard = buttonElement.dataset.preset;
+                    buttonElement.dataset.preset = "";
+                    return;
+                }
+                if (globalData.selectedCard === buttonElement.dataset.preset) {
+                    alert("Вы победили");
+                    delete globalData.selectedCard;
+                } else {
+                    alert("Вы не победили");
+                    delete globalData.selectedCard;
+                }
+            });
+        }
+    }, 5 * 1000);
 }
 export { renderingPresetsCards };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,37 @@
+import "./css/styles.css";
 import { difficultyLevel } from "./component/startingСomponent.js";
 const globalData = {
-    difficultyLevel: "",
-    cards: "",
+    suit: ["spades", "hearts", "diamonds", "clubs"],
+    rank: ["A", "K", "Q", "J", "10", "9", "8", "7", "6"],
 };
 const mainelement = document.getElementById("main");
 difficultyLevel();
+
+function getRandomInt(numb) {
+    let randomPreset = new Array(numb);
+    for (let i = 0; i < numb; i++) {
+        const randomSuit = Math.floor(Math.random() * globalData.suit.length);
+        const randomRank = Math.floor(Math.random() * globalData.rank.length);
+        randomPreset[i] =
+            globalData.suit[randomSuit] + globalData.rank[randomRank];
+    }
+    globalData.randomPreset = [...randomPreset, ...randomPreset];
+    return;
+}
+function randomButtonElements(numb) {
+    let randomButtonElement = new Array(numb);
+    for (let i = 0; i < numb; i++) {
+        const randomNumb = Math.floor(
+            Math.random() * globalData.randomPreset.length
+        );
+        randomButtonElement[i] = `
+        <button data-preset="${globalData.randomPreset[randomNumb]}" class="cards__button preset__${globalData.randomPreset[randomNumb]}"></button>
+        `;
+        globalData.randomPreset.splice(randomNumb, 1);
+    }
+    return randomButtonElement.join("");
+}
+export { getRandomInt };
 export { globalData };
 export { mainelement };
+export { randomButtonElements };
